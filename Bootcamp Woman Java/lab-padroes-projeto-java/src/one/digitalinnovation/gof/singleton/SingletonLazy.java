df9bb837174d448487af244709a2fdcbb97ed796
Caroline Lopes Avila Paier <96211668+carolpaier@users.noreply.github.com>
2022-05-03 23:49:42 -0300

    Add files via upload

---
+++ Bootcamp Woman Java/lab-padroes-projeto-java/src/one/digitalinnovation/gof/singleton/SingletonLazy.java
@@ -0,0 +1,21 @@
+package one.digitalinnovation.gof.singleton;
+/**
+ * 
+ * Singleton "preguiçoso".
+ * @author carolpaier
+ *
+ */
+public class SingletonLazy {
+
+	private static SingletonLazy instancia;
+	
+	private SingletonLazy() {
+		super();
+	}
+	public static SingletonLazy getInstancia() {
+		if (instancia == null) {
+			instancia = new SingletonLazy();
+		}
+		return instancia;
+	}
+}
